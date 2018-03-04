56e604040d1658bec5cdb59e5db275ca268f1522
Marco Heisig <marco.heisig@fau.de>
2018-03-04 22:49:17 +0100

    better detection of available PDF viewers

--- Code/Graphviz/view.lisp
+++ Code/Graphviz/view.lisp
@@ -1,6 +1,16 @@
 (in-package :cleavir-development)
 
-(defparameter *graphviz-viewer* "evince")
+(defun program-in-path-p (program)
+  (check-type program string)
+  (multiple-value-bind (out err exit-code)
+      (uiop:run-program
+       (list "which" program)
+       :ignore-error-status t)
+    (declare (ignore out err))
+    (zerop exit-code)))
+
+(defparameter *graphviz-viewer*
+  (find-if #'program-in-path-p '("evince" "okular" "xpdf")))
 
 (defclass arbitrary-cleavir-graph
     (ast
@@ -11,12 +21,12 @@
   ())
 
 (defun view (&rest graph-roots)
-  (uiop:with-temporary-file (:pathname output-file)
+  (uiop:with-temporary-file (:pathname output-file :type "pdf")
     (cl-dot:dot-graph
      (cl-dot:generate-graph-from-roots
       (make-instance 'arbitrary-cleavir-graph)
       graph-roots)
-     output-file)
+     output-file :format :pdf)
     (uiop:run-program
      (list *graphviz-viewer*
            (uiop:native-namestring output-file)))))
